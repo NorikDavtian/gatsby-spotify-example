609aded7223b15cd201370b054dd6f9747fbb8fb
Norik Davtian <norik@norik.io>
2019-03-03 23:56:38 -0800

    gatsby source spoptify example

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,13 +1,12 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
-import { StaticQuery, graphql } from 'gatsby'
+import { graphql, Link, StaticQuery } from 'gatsby'
 import { Container, Grid, Menu } from 'semantic-ui-react'
 
 import Header from './header'
 
 import 'semantic-ui-less/semantic.less'
-import { Link } from 'gatsby'
 
 const LinkedItem = ({ children, ...props }) => (
   <Menu.Item as={Link} activeClassName='active' {...props}>{children}</Menu.Item>
@@ -38,15 +37,15 @@ const Layout = ({ children, data }) => (
 
         <Container>
           <Grid relaxed stackable>
-            <Grid.Column mobile={16} tablet={4} computer={4}>
+            <Grid.Column mobile={16} computer={3}>
               <Menu vertical fluid>
                 <LinkedItem to='/' exact>Home</LinkedItem>
-                <LinkedItem to='/page-2'>Second Page</LinkedItem>
+                <LinkedItem to='/page-2'>Playlists</LinkedItem>
                 <LinkedItem to='/404'>404 Example Page</LinkedItem>
               </Menu>
             </Grid.Column>
 
-            <Grid.Column mobile={16} tablet={8} computer={8}>
+            <Grid.Column mobile={16} computer={13}>
               {children}
             </Grid.Column>
           </Grid>
